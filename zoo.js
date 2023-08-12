7bed9bdea0af315fd63e5f599b8998154e37800a
VladM <7+vistrelnikov94@gmail.com>
2023-08-12 21:17:30 +0300

    finish

--- zoo.js
+++ zoo.js
@@ -27,11 +27,9 @@ var Admin = /** @class */ (function () {
     Admin.prototype.doAction = function (action, animal, aviary) {
         switch (action) {
             case "CheckIn":
-                checkIn(animal, aviary);
-                return action;
+                return checkIn(animal, aviary);
             case "CheckOut":
-                checkOut(animal, aviary);
-                return action;
+                return checkOut(animal, aviary);
             case "CalculateAllFood":
                 return calculateAllFood(listAviary);
                 ; //I wonder why it works without an optional function parameter pointer
@@ -90,32 +88,31 @@ function checkThePossibility(animal, aviary) {
             console.log("the animal - ".concat(animal.name, " already living in the aviary - N").concat(aviaryNumber, " - ").concat(aviary.biome));
             return false;
         }
-        if (aviary.spaceAvailable <= animal.spaceDemand) {
-            console.log("space demand animal - ".concat(animal.name, " more than space available of the aviary - N").concat(aviaryNumber, " = ").concat(aviary.spaceAvailable));
+        // else if(aviary.spaceAvailable <= animal.spaceDemand){
+        //     console.log(`space demand animal - ${animal.name} more than space available of the aviary - N${aviaryNumber} = ${aviary.spaceAvailable}`);
+        //     return false;
+        // }
+        else if (animal.type.isPredator === false && aviary.animals[0].type.isPredator === true) {
+            console.log("the animal - ".concat(animal.name, " cannot live with predators"));
+            console.log("in the aviary - N".concat(aviaryNumber, " - ").concat(aviary.biome, " already living ").concat(aviary.animals[0].type.type, " predators"));
             return false;
         }
-        if (animal.type.biome !== aviary.biome) {
-            console.log("the biom - ".concat(animal.type.biome, " of the animal - ").concat(animal.name, " \n does not match biom of the aviary - N").concat(aviaryNumber, " - ").concat(aviary.biome));
-            return false;
-        }
-        if (animal.type.pondDemand === true && aviary.pondInStock === false) {
-            console.log("the aviary - N".concat(aviaryNumber, " - ").concat(aviary.biome, " hase not pond. - ").concat(animal.name, " needs pond"));
-            return false;
-        }
-        if (animal.type.isPredator === true && (aviary.animals[0].type !== animal.type)) {
-            // if(animal.type.isPredator === true && (checkPredatorsTypeInAviary(aviary) !== animal.type || checkPredatorsTypeInAviary(aviary) === true)){
-            // if(checkPredatorsTypeInAviary(aviary) !== animal.type){
+        else if (animal.type.isPredator === true && (aviary.animals[0].type !== animal.type)) {
             console.log("predators cannot live with other types of predators");
-            console.log("in the aviary - N".concat(aviaryNumber, " - ").concat(aviary.biome, " already living ").concat(aviary.animals[0].type, " predators"));
+            console.log("in the aviary - N".concat(aviaryNumber, " - ").concat(aviary.biome, " already living ").concat(aviary.animals[0].type.type, " predators"));
             return false;
-            // }
-            // else if(checkPredatorsTypeInAviary(aviary) === true){
-            //     return true;
-            // } 
         }
-        else {
-            return true;
-        }
+        // else{
+        //     return true;
+        // }
+    }
+    if (animal.type.biome != aviary.biome) {
+        console.log("the biome - ".concat(animal.type.biome, " of the animal - ").concat(animal.name, " \n does not match biom of the aviary - N").concat(aviaryNumber, " - ").concat(aviary.biome));
+        return false;
+    }
+    else if (animal.type.pondDemand === true && aviary.pondInStock == false) {
+        console.log("the aviary - N".concat(aviaryNumber, " - ").concat(aviary.biome, " hase not pond. - ").concat(animal.name, " needs pond"));
+        return false;
     }
     else {
         return true;
@@ -124,12 +121,17 @@ function checkThePossibility(animal, aviary) {
 function checkIn(animal, aviary) {
     var _a;
     if (checkThePossibility(animal, aviary)) {
+        if (aviary.spaceAvailable <= animal.spaceDemand) {
+            console.log("space demand animal - ".concat(animal.name, " more than space available of the aviary - N").concat(listAviary.indexOf(aviary), " = ").concat(aviary.spaceAvailable));
+            return false;
+        }
         aviary.spaceAvailable -= animal.spaceDemand;
         (_a = aviary.animals) === null || _a === void 0 ? void 0 : _a.push(animal);
-        return "the animal - ".concat(animal.name, " settled in the aviary - N").concat(listAviary.indexOf(aviary), ". \n Now - available space = ").concat(aviary.biome);
+        console.log("the animal - ".concat(animal.name, " settled in the aviary - N").concat(listAviary.indexOf(aviary), ". \n Now - available space = ").concat(aviary.spaceAvailable));
+        return aviary;
     }
     else {
-        return;
+        return "try another one aviary for the animal - ".concat(animal.name);
     }
 }
 function checkOut(animal, aviary) {
@@ -137,6 +139,7 @@ function checkOut(animal, aviary) {
     if ((_a = aviary.animals) === null || _a === void 0 ? void 0 : _a.some(function (x) { return x == animal; })) {
         delete aviary.animals[(_b = aviary.animals) === null || _b === void 0 ? void 0 : _b.indexOf(animal)];
         aviary.spaceAvailable += animal.spaceDemand;
+        aviary.animals.sort(function (a, b) { return (a.name < b.name ? -1 : 1); });
         console.log("Ok ".concat(animal.name, " moved out from ").concat(listAviary.indexOf(aviary), " - ").concat(aviary.biome));
         return aviary;
     }
@@ -156,15 +159,30 @@ function calculateAllFood(listAviary) {
 }
 var dog = createAnimalType("Dog", "House", false, true);
 var cat = createAnimalType("Cat", "House", false, true);
+var cow = createAnimalType("Cow", "House", false, false);
 var hippopotamus = createAnimalType("Hippopotamus", "Tropics", true, false);
 var dogSasa = createAcceptedAnimal(dog, "Sasa", 5, 3);
 var dogVova = createAcceptedAnimal(dog, "Vova", 5, 3);
+var dogLiza = createAcceptedAnimal(dog, "Liza", 5, 3);
+var cowMusa = createAcceptedAnimal(cow, "Musa", 5, 1);
 var catMasha = createAcceptedAnimal(cat, "Masha", 4, 2);
 var hippopotamusSerj = createAcceptedAnimal(hippopotamus, "Serj", 80, 20);
 var tropicsAviary = createAviary("Tropics", false, 600);
-var houseAviary = createAviary("House", false, 600);
+var tropicsAviaryWithoutPound = createAviary("Tropics", false, 600);
+var tropicsAviarySmallSize = createAviary("Tropics", false, 6);
+var houseAviary = createAviary("House", false, 11);
 var admin = new Admin();
+// admin.doAction("CheckIn", cowMusa, houseAviary);
+// admin.doAction("CheckIn", hippopotamusSerj, houseAviary);
+// admin.doAction("CheckIn", hippopotamusSerj, houseAviary);
+// admin.doAction("CheckIn", hippopotamusSerj, tropicsAviaryWithoutPound);
+// admin.doAction("CheckIn", hippopotamusSerj, tropicsAviarySmallSize);
+// admin.doAction("CheckIn", hippopotamusSerj, tropicsAviary);
+// admin.doAction("CheckIn", dogSasa, tropicsAviary);
 // admin.doAction("CheckIn", dogSasa, houseAviary);
-// houseAviary.animals?.push(dogSasa);
-// houseAviary.animals?.push(catMasha);
-// tropicsAviary.animals?.push(dogSasa);
+// admin.doAction("CheckIn", dogSasa, houseAviary);
+// admin.doAction("CheckIn", catMasha, houseAviary);
+// admin.doAction("CheckIn", dogVova, houseAviary);
+// admin.doAction("CheckOut", dogSasa, houseAviary);
+// admin.doAction("CheckOut", cowMusa, houseAviary);
+// admin.doAction("CheckIn", cowMusa, houseAviary);
--- zoo.ts
+++ zoo.ts
@@ -19,7 +19,6 @@ interface AcceptedAnimal {
 interface Aviary {
     biome: TypeOfBiome;
     pondInStock: boolean;
-    existPredators: boolean;
     readonly spaceInTotal: number;
     spaceAvailable: number;
     animals: AcceptedAnimal[];
@@ -35,11 +34,9 @@ class Admin {
     doAction(action: AdminAction, animal: AcceptedAnimal, aviary: Aviary) {
         switch (action) {
             case "CheckIn":
-                checkIn(animal, aviary);
-                return action;
+                return checkIn(animal, aviary);
             case "CheckOut":
-                checkOut(animal, aviary);
-                return action;
+                return checkOut(animal, aviary);
             case "CalculateAllFood":
                 return calculateAllFood(listAviary);; //I wonder why it works without an optional function parameter pointer
             default:
@@ -100,7 +97,7 @@ function createAviary(
 
 function checkPredatorsTypeInAviary(aviary: Aviary) {
     var animals = aviary.animals;
-    if(aviary.animals.length !== 0){
+    if(aviary.animals.length !== 0) {
         return animals[0].type;
     }
     else return true;
@@ -113,45 +110,48 @@ function checkThePossibility(animal: AcceptedAnimal, aviary: Aviary): boolean {
     console.log(`## are you trying to settle animal - ${animal.name} in the aviary - N${aviaryNumber} - ${aviary.biome} ##`);
     console.log("#####################################################");
     
-    if(aviary.animals.length !== 0){
+    if(aviary.animals.length !== 0) {
         if(aviary.animals?.some(x=>x==animal)) { // I did not do the implementation of the search in the list of aviary
             console.log(`the animal - ${animal.name} already living in the aviary - N${aviaryNumber} - ${aviary.biome}`);
             return false;
         }
-        if(aviary.spaceAvailable <= animal.spaceDemand){
-            console.log(`space demand animal - ${animal.name} more than space available of the aviary - N${aviaryNumber} = ${aviary.spaceAvailable}`);
+        else if(animal.type.isPredator === false && aviary.animals[0].type.isPredator === true) {
+            console.log(`the animal - ${animal.name} cannot live with predators`);
+            console.log(`in the aviary - N${aviaryNumber} - ${aviary.biome} already living ${aviary.animals[0].type.type} predators`);
             return false;
         }
-        if(animal.type.biome !== aviary.biome) {
-            console.log(`the biom - ${animal.type.biome} of the animal - ${animal.name} \n does not match biom of the aviary - N${aviaryNumber} - ${aviary.biome}`);
-            return false;
-        }
-        if(animal.type.pondDemand === true && aviary.pondInStock === false) {
-            console.log(`the aviary - N${aviaryNumber} - ${aviary.biome} hase not pond. - ${animal.name} needs pond`);
-            return false;
-        }
-        if(animal.type.isPredator === true && (aviary.animals[0].type !== animal.type)){
+        else if(animal.type.isPredator === true && (aviary.animals[0].type !== animal.type)) {
             console.log(`predators cannot live with other types of predators`);
-            console.log(`in the aviary - N${aviaryNumber} - ${aviary.biome} already living ${aviary.animals[0].type} predators`);
+            console.log(`in the aviary - N${aviaryNumber} - ${aviary.biome} already living ${aviary.animals[0].type.type} predators`);
             return false;
-        }
-        else{
-            return true;
         }
     }
-    else{
+    if(animal.type.biome != aviary.biome) {
+        console.log(`the biome - ${animal.type.biome} of the animal - ${animal.name} \n does not match biom of the aviary - N${aviaryNumber} - ${aviary.biome}`);
+        return false;
+    }
+    else if(animal.type.pondDemand === true && aviary.pondInStock == false) {
+        console.log(`the aviary - N${aviaryNumber} - ${aviary.biome} hase not pond. - ${animal.name} needs pond`);
+        return false;
+    }
+    else {
         return true;
     }
 }
 
 function checkIn(animal: AcceptedAnimal, aviary: Aviary) {
-    if(checkThePossibility(animal, aviary)){
+    if(checkThePossibility(animal, aviary)) {
+        if(aviary.spaceAvailable <= animal.spaceDemand){
+            console.log(`space demand animal - ${animal.name} = ${animal.spaceDemand} more than space available of the aviary - N${listAviary.indexOf(aviary)} = ${aviary.spaceAvailable}`);
+            return false;
+        }
         aviary.spaceAvailable -= animal.spaceDemand;
         aviary.animals?.push(animal);
-        return `the animal - ${animal.name} settled in the aviary - N${listAviary.indexOf(aviary)}. \n Now - available space = ${aviary.biome}`;
+        console.log(`the animal - ${animal.name} settled in the aviary - N${listAviary.indexOf(aviary)}. \n Now - available space = ${aviary.spaceAvailable}`);
+        return aviary;
     }
-    else{
-        return
+    else {
+        return `try another one aviary for the animal - ${animal.name}`;
     }
 }
 
@@ -159,6 +159,7 @@ function checkOut(animal: AcceptedAnimal, aviary: Aviary) {
     if(aviary.animals?.some(x=>x==animal)){
         delete aviary.animals[aviary.animals?.indexOf(animal)];
         aviary.spaceAvailable += animal.spaceDemand;
+        aviary.animals.sort((a, b) => (a.name < b.name ? -1 : 1));
         console.log(`Ok ${animal.name} moved out from ${listAviary.indexOf(aviary)} - ${aviary.biome}`);
 
         return aviary;
@@ -180,20 +181,35 @@ function calculateAllFood(listAviary: Array<Aviary>) {
 
 const dog: AnimalType = createAnimalType("Dog", "House", false, true);
 const cat: AnimalType = createAnimalType("Cat", "House", false, true);
+const cow: AnimalType = createAnimalType("Cow", "House", false, false);
 const hippopotamus: AnimalType = createAnimalType("Hippopotamus", "Tropics", true, false);
 
 let dogSasa = createAcceptedAnimal(dog, "Sasa", 5, 3);
-let dogVova = createAcceptedAnimal(dog, "Vova", 5, 3);        
+let dogVova = createAcceptedAnimal(dog, "Vova", 5, 3);
+let dogLiza = createAcceptedAnimal(dog, "Liza", 5, 3);
+let cowMusa = createAcceptedAnimal(cow, "Musa", 5, 1);        
 let catMasha = createAcceptedAnimal(cat, "Masha", 4, 2);
 let hippopotamusSerj = createAcceptedAnimal(hippopotamus, "Serj", 80, 20);
 
 let tropicsAviary = createAviary("Tropics", false, 600);
+let tropicsAviaryWithoutPound = createAviary("Tropics", false, 600);
+let tropicsAviarySmallSize = createAviary("Tropics", false, 6);
 let houseAviary = createAviary("House", false, 11);
 
 let admin = new Admin();
 
-// admin.doAction("CheckIn", dogSasa, houseAviary);
+admin.doAction("CheckIn", cowMusa, houseAviary);
+admin.doAction("CheckIn", hippopotamusSerj, houseAviary);
+admin.doAction("CheckIn", hippopotamusSerj, houseAviary);
+admin.doAction("CheckIn", hippopotamusSerj, tropicsAviaryWithoutPound);
+admin.doAction("CheckIn", hippopotamusSerj, tropicsAviarySmallSize);
 
-// houseAviary.animals?.push(dogSasa);
-// houseAviary.animals?.push(catMasha);
-// tropicsAviary.animals?.push(dogSasa);
+admin.doAction("CheckIn", hippopotamusSerj, tropicsAviary);
+admin.doAction("CheckIn", dogSasa, tropicsAviary);
+admin.doAction("CheckIn", dogSasa, houseAviary);
+admin.doAction("CheckIn", dogSasa, houseAviary);
+admin.doAction("CheckIn", catMasha, houseAviary);
+admin.doAction("CheckIn", dogVova, houseAviary);
+admin.doAction("CheckOut", dogSasa, houseAviary);
+admin.doAction("CheckOut", cowMusa, houseAviary);
+admin.doAction("CheckIn", cowMusa, houseAviary);
